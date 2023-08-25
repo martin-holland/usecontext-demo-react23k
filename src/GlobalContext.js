22337b98f7a0a4e2b9c37b4406c7d7b2da93d71f
Martin Holland <martin.holland@free.fi>
2023-08-25 12:09:30 +0300

    useContext demo playground showing how to set up and useContext from React

---
+++ src/GlobalContext.js
@@ -0,0 +1,13 @@
+import { createContext } from 'react';
+export const GlobalContext = createContext();
+
+const GlobalContextProvider = (props) => {
+
+    return (
+        <GlobalContext.Provider value={"TEST ABC"}>
+            {props.children}
+        </GlobalContext.Provider>
+    )
+}
+
+export default GlobalContextProvider;
